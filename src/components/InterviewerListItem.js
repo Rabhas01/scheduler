7be5c1e73ec937f8d2b09620a1d4a06fcd4de172
Priscilla <rabhas.vashisht@gmail.com>
2022-04-21 09:58:22 -0400

    replace hard-coded values with props

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,14 +1,18 @@
 import React from "react";
 import "components/InterviewerListItem.scss" 
+import classNames from "classnames";
 
-export default function InterviewerListItem() {
-
+export default function InterviewerListItem(props) {
+  const InterviewClass = classNames('interviewers__item', {
+    'interviewers__item--selected':props.selected
+  });
+  
   return(
-    <li className="interviewers__item">
+    <li className={InterviewClass} onClick={() => props.setInterviewer(props.id)} >
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        src={props.avatar}
+        alt={props.name}
       />
   Sylvia Palmer
 </li>
