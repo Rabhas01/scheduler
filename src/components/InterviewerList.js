9785187be9f42e322c47c0a3ab91c4e8a9f92c9c
Priscilla <rabhas.vashisht@gmail.com>
2022-04-21 10:15:12 -0400

    create interviewerList and styling file

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "components/interviewer.scss"
+
+export default function interviewer() {
+
+  return(
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list"></ul>
+    </section>
+  )
+}
