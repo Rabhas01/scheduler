f83405058c33a20779d4c03a0064e004a407d5bf
Priscilla <rabhas.vashisht@gmail.com>
2022-04-21 16:33:48 -0400

    Update the InterviewList Stories

--- stories/index.js
+++ stories/index.js
@@ -126,9 +126,11 @@ storiesOf("DayListItem", module)
             interviewer={3}
           />
         ))
+        
         .add("Clickable", () => (
-          <InterviewerList
-            interviewers={interviewers}
-            setInterviewer={action("setInterviewer")}
+          <InterviewerListItem
+            name={interviewer.name}
+            avatar={interviewer.avatar}
+            setInterviewer={() => action("setInterviewer")(interviewer.id)}
           />
         ));
