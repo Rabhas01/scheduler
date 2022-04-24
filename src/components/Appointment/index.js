79c3a05f82df4682d5e8f8e97bb17f17f7852492
Priscilla <rabhas.vashisht@gmail.com>
2022-04-23 21:37:09 -0400

    Refactor Appointment component and story

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "components/Appointment/style.scss";
-import "components/Appointment/Header.js"
+import Header from "./Header";
 
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
 
   return(
     <article className="appointment">
-      {props.time && `Appointment at ${props.time}`}
+      <Header time={props.time} />
+      {props.interview ?
+       <Show
+       student={props.interview.student}
+       interviewer={props.interview.interviewer.name}
+       /> : <Empty />}
     </article>
 
   );
