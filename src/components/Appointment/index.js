2527d996bbe73952901641f8e25af7720b72e2ce
Priscilla <rabhas.vashisht@gmail.com>
2022-04-24 09:48:32 -0400

    Add appointment data to application.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,10 @@
 import React from "react";
-import "components/Appointment/style.scss";
+import "./style.scss";
 import Header from "./Header";
-
 import Show from "./Show";
 import Empty from "./Empty";
+
+
 
 export default function Appointment(props) {
 
