d6d8444f7efaa72417208f1f66d7a199e519d5b6
Priscilla <rabhas.vashisht@gmail.com>
2022-04-24 19:42:37 -0400

    make multiple axios request to get appointment data from the api

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,47 +1,10 @@
 import React, { useState, useEffect } from "react";
 import "components/Application.scss";
 import DayList from "components/DayList";
-import  Appointment from "./Appointment";
+import Appointment from "./Appointment";
 import axios from "axios";
+import { getAppointmentsForDay } from "helpers/selectors";
 
-// const appointments = {
-//   "1": {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   "2": {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer:{
-//         id: 3,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   "3": {
-//     id: 3,
-//     time: "2pm",
-//   },
-//   "4": {
-//     id: 4,
-//     time: "3pm",
-//     interview: {
-//       student: "Archie Andrews",
-//       interviewer:{
-//         id: 4,
-//         name: "Cohana Roy",
-//         avatar: "https://i.imgur.com/FK8V841.jpg",
-//       }
-//     }
-//   },
-//   "5": {
-//     id: 5,
-//     time: "4pm",
-//   }
-// };
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -50,18 +13,18 @@ export default function Application(props) {
     appointments: {}
   });
 
-  const dailyAppointments = [];
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
   const setDay = day => setState({ ...state, day });
   
-  
   useEffect(() => {
-    axios
-    .get('/api/days')
-    .then(response => {
-    
-    setDays([...response.data]);
-    });
-    },[])
+
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments')
+    ]).then((all) => {
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}))
+   });
+  },[])
     
   
   return (
@@ -99,4 +62,4 @@ export default function Application(props) {
       </section>
     </main>
   );
-}
+  }
