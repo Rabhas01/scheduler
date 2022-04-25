37816b6e63d7d2ddc90de36e4450e73781850b49
Priscilla <rabhas.vashisht@gmail.com>
2022-04-25 06:39:10 -0400

    retrieve INterviewer Data by making api request

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -11,3 +11,22 @@ export function getAppointmentsForDay(state, day) {
   }
   return appointmentsForDay;
 }
+
+export function getInterview(state, interview){
+
+  if (interview === null) {
+    return null;
+  }
+
+  const interviewData = {
+    student: interview.student,
+    interviewer: {
+      id: state.interviewers[interview.interviewer].id, 
+      name: state.interviewers[interview.interviewer].name,
+      avatar: state.interviewers[interview.interviewer].avatar  
+    }
+  }
+  return interviewData;
+}
+
+
