f2ddf384c82285dc28b3e488d8110b416b9454cb
Priscilla <rabhas.vashisht@gmail.com>
2022-04-25 16:52:27 -0400

    create and add selector for interviews

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -44,38 +44,3 @@ export function getInterviewersForDay(state, day) {
   return interviewForDay;
 }
 
-// export function getInterviewersForDay(state, day) {
-//   const filteredInterviewers = [];
-
-//   if (state.days.length !== 0){
-//     const daySelected = state.days.find(daysArr => daysArr.name === day);
-//     if (daySelected) {
-//       for (const apt of daySelected.interviewers) {
-//         filteredInterviewers.push(state.interviewers[apt])
-//       }
-//     }
-//   }
-//   return filteredInterviewers;
-// } 
-
-// export function getInterviewersForDay(state, day) {
-//   // Get only the day for which the name matches the day prop
-//   const selectedDay = state.days.filter(d => d.name === day)[0];
-
-//   const interviewersForDay = [];
-//   // Init interviewers to return and check if no days were found (skip if so)
-//   if (selectedDay) {
-//     // Iterate over interviewers list for the selected day, and
-//     //   return matching interviewers
-//     for (const a of selectedDay.appointments) {
-//       const foundInterview = state.appointments[a].interview;
-//       const foundInterviewerId = foundInterview ? foundInterview.interviewer : undefined;
-//       const foundInterviewer = state.interviewers[foundInterviewerId];
-//       if (foundInterviewer) { 
-//         interviewersForDay.push(foundInterviewer);
-//       }
-//     }
-//   }
-
-//   return interviewersForDay;
-// }
