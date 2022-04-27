c781afd91790879c46e174e324b035b5cf8bb349
Priscilla <rabhas.vashisht@gmail.com>
2022-04-27 08:35:23 -0400

    useApplicationData to organize app logic

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,65 +2,21 @@ import React, { useState, useEffect } from "react";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "./Appointment";
-import axios from "axios";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
+import useApplicationData from "hooks/useApplicationData"
 
 export default function Application(props) {
+ const {state, setDay, bookInterview, cancelInterview} = useApplicationData
 
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments:{},
-    interviewers:{}
-  });
-  const setDay = day => setState({ ...state, day });
-  
-  useEffect(() => {
-
-    Promise.all([
-      axios.get('/api/days'),
-      axios.get('/api/appointments'),
-      axios.get('/api/interviewers')
-    ])
-    .then(all => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}))
-   });
-  },[]);
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
   const dailyInterviewrs = getInterviewersForDay(state, state.day);
   
   const schedule = dailyAppointments.map(appointment => { 
   const interview = getInterview(state, appointment.interview);
-
-    
-
-  
-    function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    axios.put(`/api/appointments/${id}`, { interview })
-    .then (
-      setState({
-      ...state,
-      appointments
-    }));
    
-  }
-    
-  function cancelInterview(id){
-    
-    axios.delete(`/api/appointments/${id}`)
-    .then (
-    );
-  };
   
+ 
     return (
       <Appointment
         key={appointment.id}
@@ -102,4 +58,4 @@ export default function Application(props) {
       </section>
     </main>
   );
-  }
+}
