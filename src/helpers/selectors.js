4744141e92c2544f6e9491ef96605b2b705058ee
Priscilla <rabhas.vashisht@gmail.com>
2022-05-06 12:57:22 -0400

    refactor helper function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,17 +1,13 @@
 export function getAppointmentsForDay(state, day) {
-  const appointmentsForDay = [];  
-  const chosenDay = state.days.length
-  if (chosenDay !== 0 ){
-  const selectedDay = state.days.find(dayArry => dayArry.name === day);
-  if (selectedDay) {
-    for (const appt of selectedDay.appointments){
-      appointmentsForDay.push(state.appointments[appt])
-      }
+  const chosenDay = state.days.length;
+  if (chosenDay) {
+    const selectedDay = state.days.find((dayArry) => dayArry.name === day);
+    if (selectedDay) {
+      return selectedDay.appointments.map((appt) => state.appointments[appt]);
     }
   }
-  return appointmentsForDay;
+  return [];
 }
-
 export function getInterview(state, interview){
 
   if (interview === null) {
