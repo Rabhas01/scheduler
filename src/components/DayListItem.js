98ddb7e948db4c52762e4ccb79cfd94ef8f10876
Priscilla <rabhas.vashisht@gmail.com>
2022-05-06 23:30:02 -0400

    remove the commented out code from DaylistItem fn

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,66 +1,27 @@
-// import React from "react";
-// import classNames from "classnames";
-// import "components/DayListItem.scss"; 
-
-// export default function DayListItem(props) {
-// const DayListItem = classNames('day-list__item', {
-//   'day-list__item--selected':props.selected, 'day-list__item--full': props.spots === 0
-// });
-
-// function formatSpots(props) {
-//   if (props.spots === 0) {
-//     return `no spots remaining`
-//   }
-//   if (props.spots === 1) {
-//     return `1 spot remaining`
-//   }
-//   return `${props.spots} spots remaining`
-// }
-
-//   return (
-//     <li className={DayListItem} onClick={() => props.setDay(props.name)}>
-//       <h2 className="text--regular">{props.name}</h2> 
-//       <h3 className="text--light">{formatSpots(props)}</h3>
-//       </li>
-//   );
-// }
-
-
-import classNames from "classnames";
 import React from "react";
-import "components/DayListItem.scss"
+import classNames from "classnames";
+import "components/DayListItem.scss"; 
 
 export default function DayListItem(props) {
-  let dayClass = classNames('day-list__item',
-  {"day-list__item--selected": props.selected},
-  {"day-list__item--full": props.spots === 0})
+const DayListItem = classNames('day-list__item', {
+  'day-list__item--selected':props.selected, 'day-list__item--full': props.spots === 0
+});
 
-  let spotsRem = ''
-
-  const formatSpots = () => {
-    if (props.spots === 0) {
-      spotsRem = 'no spots remaining'
-    }
-    if (props.spots === 1) {
-      spotsRem = '1 spot remaining'
-    }
-    if (props.spots > 1) {
-      spotsRem = props.spots + ' spots remaining'
-    }
+function formatSpots(props) {
+  if (props.spots === 0) {
+    return `no spots remaining`
   }
-
-  formatSpots();
+  if (props.spots === 1) {
+    return `1 spot remaining`
+  }
+  return `${props.spots} spots remaining`
+}
 
   return (
-
-      <li
-      onClick={props.setDay}
-      className={dayClass}
-      data-testid="day"
-      >
-        <h2 className="text--regular">{props.name}</h2>
-        <h3 className="text--light">{spotsRem}</h3>
+    <li className={DayListItem} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{formatSpots(props)}</h3>
       </li>
-
   );
 }
+
